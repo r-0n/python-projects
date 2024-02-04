55f5d5408b38fcacf032164c7418effeecdb1902
r-0n <ronywajah911@gmail.com>
2024-02-04 21:37:11 +0000

    update

--- Battelshipgame.py
+++ Battelshipgame.py
@@ -1,5 +1,5 @@
 #Author: Aaron Wajah
-#Description: A game of tik-tak-toe
+#Description: Battleship game
 
 #Update: user input not working
 
@@ -21,12 +21,18 @@ AJ =list((string.ascii_lowercase[:10].upper()))# column header from A to J
 
 
 
+
+
+
+
 #creating the 10x10 board for simulation
+
 for row in range(rows_and_column):
     rowlist = []
     for col in range(rows_and_column):
         rowlist.append(" ")
     game.append(rowlist)
+
 
 for col_num in range(rows_and_column):
     print("   "+ AJ[col_num],end =" ")
@@ -39,6 +45,8 @@ for row in range(rows_and_column):
         print(game[row][col] + " | ", end=" ")
     print("\n +"+"----+"*rows_and_column)
 
+
+
 print("Prepare to Play battleship👾👾")
 time.sleep(1)
 #Placing the random ship on the grid
@@ -50,6 +58,7 @@ print(ship_row,ship_column)
 while ship_row > rows_and_column-4 and ship_column> rows_and_column-4:
     ship_row= random.randint(0,rows_and_column-1)
     ship_column= random.randint(0,rows_and_column-1)
+
 
 
 #check whether the ship should be placed on board vertically
@@ -84,8 +93,8 @@ guess_count=0
 guess_column = input("Guess Column Letter")
 guess_row = int(input("Guess Row Number"))
 
-wrong_input = False
-while wrong_input:
+#wrong_input = False
+while True:
     if guess_column not in AJ:
         print("please input a Capital letter for COLUMN and number for row")
     elif guess_row > (rows_and_column-1):
@@ -95,13 +104,50 @@ while wrong_input:
     else:
         if guess_column == AJ[ship_column] and guess_row== ship_row:
             game[guess_column][guess_row].append("X")
+            
+            #display after ship has been hit
+            for col_num in range(rows_and_column):
+                print("   "+ AJ[col_num],end =" ")   
+            print("\n +"+"----+"* rows_and_column)
+
+            for row in range(rows_and_column):
+                print(str(row)+"| ", end =" ")
+                for col in range(rows_and_column):
+                    if "X" in game[row][col]:
+                        print(game[row][col] + " | ", end=" ")
+                    else:
+                        game[row][col]=" "
+                        print(game[row][col] + " | ", end=" ")
+                print("\n +"+"----+"*rows_and_column)
+            
+
+
             guess_count+=1
             if guess_count == 4:
                 print("Congrats, You sunk the ship man!!")
                 break
         else:
             game[guess_column][guess_row].append("#")  
+
+            #display when ship has been missed
+            for col_num in range(rows_and_column):
+                print("   "+ AJ[col_num],end =" ")   
+            print("\n +"+"----+"* rows_and_column)
+
+            for row in range(rows_and_column):
+                print(str(row)+"| ", end =" ")
+                for col in range(rows_and_column):
+                    if "#" in game[row][col]:
+                        print(game[row][col] + " | ", end=" ")
+                    else:
+                        game[row][col]=" "
+                        print(game[row][col] + " | ", end=" ")
+                print("\n +"+"----+"*rows_and_column)
             guess_count+=1
+
+
+
+    
     
 
 for row in range(rows_and_column):
